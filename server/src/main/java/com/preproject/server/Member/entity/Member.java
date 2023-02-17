bac463dda4c4a8ffdc50cf36bbfdee1712254deb
SigLee <lgh941107@gmail.com>
2023-02-18 01:27:49 +0900

    [back] rename: Member Entity 필드 이름 변경
    필드 nickName -> displayName
    
    contributors: LEEGIHO94

--- server/src/main/java/com/preproject/server/Member/entity/Member.java
+++ server/src/main/java/com/preproject/server/Member/entity/Member.java
@@ -9,11 +9,12 @@ import lombok.*;
 import javax.persistence.*;
 
 import static javax.persistence.EnumType.*;
+import static lombok.AccessLevel.*;
 
 @Entity
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 @Getter
 @ToString
 public class Member extends BaseEntity {
@@ -22,7 +23,7 @@ public class Member extends BaseEntity {
     @Column(name = "member_id")
     private Long id;
     private String email;
-    private String nickName;
+    private String displayName;
     private String password;
     private String profile;
     private String location;
