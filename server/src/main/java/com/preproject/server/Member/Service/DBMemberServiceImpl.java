3447bb8f9b2f66a8175c2a6d5208466322691dcd
SigLee <lgh941107@gmail.com>
2023-02-19 02:10:50 +0900

    [back] rename: MemberService 이름 변경
    MemberService.java -> MemberServiceImpl
    
    contributors: LEEGIHO94

---
+++ server/src/main/java/com/preproject/server/Member/Service/DBMemberServiceImpl.java
@@ -0,0 +1,12 @@
+package com.preproject.server.Member.Service;
+
+import lombok.RequiredArgsConstructor;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+public class DBMemberServiceImpl implements MemberService {
+
+
+}
