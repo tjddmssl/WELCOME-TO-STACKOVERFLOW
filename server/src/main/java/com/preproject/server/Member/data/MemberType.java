7537d8d760ae9ab6f83c79ac67a5f5b75f7b8286
SigLee <lgh941107@gmail.com>
2023-02-19 04:14:24 +0900

    [back] fit: MemberType 데이터 수정
    security 적용을 위한 데이터 수정
    
    contributors: LEEGIHO94

--- server/src/main/java/com/preproject/server/Member/data/MemberType.java
+++ server/src/main/java/com/preproject/server/Member/data/MemberType.java
@@ -3,16 +3,16 @@ package com.preproject.server.Member.data;
 import lombok.Getter;
 
 public enum MemberType {
-    MEMBER_USER(1,"일반 회원") , MEMBER_ADMIN(2,"관리자");
+    ROLE_USER(1,"일반 회원") , ROLE_ADMIN(2,"관리자");
 
     @Getter
     private int index;
 
     @Getter
-    private String name;
+    private String description;
 
-    MemberType(int index, String name) {
+    MemberType(int index, String description) {
         this.index = index;
-        this.name = name;
+        this.description = description;
     }
 }
