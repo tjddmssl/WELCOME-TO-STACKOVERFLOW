dd47666073196258e16540dbcea070ed7040fe31
SigLee <lgh941107@gmail.com>
2023-02-19 23:31:49 +0900

    [back] feat: 권한 부여 메서드 작성
    권한부여 메서드 작성
    
    contributors: LEEGIHO94

--- server/src/main/java/com/preproject/server/Member/utils/CustomAuthorityUtils.java
+++ server/src/main/java/com/preproject/server/Member/utils/CustomAuthorityUtils.java
@@ -1,25 +1,39 @@
 package com.preproject.server.Member.utils;
 
+import com.preproject.server.Member.data.MemberType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.preproject.server.Member.data.MemberType.*;
 
 @Component
 public class CustomAuthorityUtils {
     @Value("${mail.address.admin}")
     private String adminMailAddress;
 
-    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN","ROLE_USER");
-    private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_USER");
+    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList(ROLE_ADMIN.name(), ROLE_USER.name());
+    private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList(ROLE_USER.name());
 
-    public List<GrantedAuthority> createAuthorities(String email) {
+    private final List<String> ADMIN_ROLES_STRING = List.of("ADMIN", "USER");
+    private final List<String> USER_ROLES_STRING = List.of("USER");
+
+    public List<String> createRoles(String email) {
         if (email.equals(adminMailAddress)) {
-            return ADMIN_ROLES;
+            return ADMIN_ROLES_STRING;
         } else {
-            return USER_ROLES;
+            return USER_ROLES_STRING;
         }
+    }
+    public List<GrantedAuthority> createAuthorities(List<String> roles) {
+        List<GrantedAuthority> authorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+        return authorities;
     }
 }
