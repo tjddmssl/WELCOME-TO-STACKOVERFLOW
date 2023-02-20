050214236a79a78dd1f9e9d9603448f6926d8132
SigLee <lgh941107@gmail.com>
2023-02-20 12:59:16 +0900

    [back] refactory:  MemberRepository 코드 추가 MemberRepository 코드 추가
    contributors: LEEGIHO94

--- server/src/main/java/com/preproject/server/Member/repository/MemberRepository.java
+++ server/src/main/java/com/preproject/server/Member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.preproject.server.Member.repository;
 import com.preproject.server.Member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
