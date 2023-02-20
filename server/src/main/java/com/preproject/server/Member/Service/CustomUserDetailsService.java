8a1e077b121ba7c225fd7882f4e48e2755a0d528
SigLee <lgh941107@gmail.com>
2023-02-20 10:57:24 +0900

    [back] feat: 서비스 로직 추가
    contributors: LEEGIHO94

--- server/src/main/java/com/preproject/server/Member/Service/CustomUserDetailsService.java
+++ server/src/main/java/com/preproject/server/Member/Service/CustomUserDetailsService.java
@@ -23,50 +23,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member findMember = memberRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
-        return CustomUserDetail.builder()
-                .id(findMember.getId())
-                .displayName(findMember.getDisplayName())
-                .password(findMember.getPassword())
-                .aboutMe(findMember.getAboutMe())
-                .email(findMember.getEmail())
-                .profile(findMember.getProfile())
-                .location(findMember.getLocation())
-                .memberStatus(findMember.getMemberStatus())
-                .build();
+        return new CustomUserDetail(findMember,authorityUtils);
     }
 
-    //부모 자식 모두 superbuilder 써야 자식 클래스로만들어짐 -> superBuilder 찾아보기
-    @SuperBuilder
-    private final class CustomUserDetail extends Member implements UserDetails {
 
-        @Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-            return authorityUtils.createAuthorities(this.getEmail());
-        }
-
-        @Override
-        public String getUsername() {
-            return this.getEmail();
-        }
-
-        @Override
-        public boolean isAccountNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isAccountNonLocked() {
-            return true;
-        }
-
-        @Override
-        public boolean isCredentialsNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isEnabled() {
-            return true;
-        }
-    }
 }
