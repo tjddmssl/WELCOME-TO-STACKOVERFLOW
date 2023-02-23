efbafb434bc4c46bde59d1f21964b5862fc387f4
tjddmssl <tjddmssl910103@gmail.com>
2023-02-23 12:13:51 +0900

    [front] feat : reduxtoolkit-setting 리덕스툴킷, 데이터 파일만듬 contributor:tjddmssl

--- client/src/components/TopQLists.js
+++ client/src/components/TopQLists.js
@@ -1,9 +1,32 @@
 import TopQList from './TopQList';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import getTopQListSlice from '../components/redux/slice/getTopQListSlice';
+import axios from 'axios';
+
 //* 질문 리스트 조건부 렌더링하기
 function TopQLists() {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => {
+    return state.getTopQList.content;
+  });
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/content');
+        dispatch(getTopQListSlice.actions.get(response.data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div>
-      <TopQList />
+      {data.map((el) => {
+        return <TopQList key={el.id} content={el} />;
+      })}
     </div>
   );
 }
