d9da429e308fab4cadaaead5a77b413e56c764c3
ImGyuriKim <gyurikim0105@gmail.com>
2023-02-23 13:10:17 +0900

    [front] 리덕스, globalstyle 파일 경로 수정 리덕스, globalstyle src하위로 옮김 contributors: ImGyuriKim

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import GlobalStyle from './components/GlobalStyle';
+import GlobalStyle from './GlobalStyle';
 import { Provider } from 'react-redux';
-import store from './components/redux/store/getTopQListStore';
+import store from './redux/store/getTopQListStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
