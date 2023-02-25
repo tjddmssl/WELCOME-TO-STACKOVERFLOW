2bcbf6ab0a461b706e2fe37d43e24ac38bcef5d1
ImGyuriKim <111730140+ImGyuriKim@users.noreply.github.com>
2023-02-25 15:04:13 +0900

    Front feat/ask post (#53)
    * [front] feat:ask_post
    ask페이지 타이틀 컨텐트 태그 post 하는중
    contributor:tjddmssl
    
    * [front]feat: ask post
    post성공, 홈화면으로 가면서 추가돼기 성공, 태그 구현 성공
    contributor:tjddmssl
    
    ---------
    
    Co-authored-by: tjddmssl <tjddmssl910103@gmail.com>

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -3,12 +3,14 @@ import getQViewSlice from '../slice/getQView';
 import getAnswerSlice from '../slice/getAnswer';
 import getTopQListSlice from '../slice/getTopQListSlice';
 import getTagsSLice from '../slice/getTags';
+import getQuestionSlice from '../slice/getQuestionSlice';
 const store = configureStore({
   reducer: {
     getTopQList: getTopQListSlice.reducer,
     getQView: getQViewSlice.reducer,
     getAnswer: getAnswerSlice.reducer,
     getTags: getTagsSLice.reducer,
+    getQuestion: getQuestionSlice.reducer,
   },
 });
 
