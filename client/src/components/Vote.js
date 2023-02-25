441005f74ad409bea69ee44b1b7df5ecf28adab5
ImGyuriKim <gyurikim0105@gmail.com>
2023-02-25 15:57:27 +0900

    [front] feat: 좋아요 버튼 구현 좋아요 버튼 및 아이콘 생성 contributors: ImGyuriKim

--- client/src/components/Vote.js
+++ client/src/components/Vote.js
@@ -1,5 +1,57 @@
+import { GoTriangleUp, GoTriangleDown } from 'react-icons/go';
+import { FiBookmark } from 'react-icons/fi';
+import { RxCountdownTimer } from 'react-icons/rx';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 10px;
+  button {
+    border: none;
+    background: transparent;
+    font-size: 45px;
+    color: #babfc4;
+  }
+  button:active {
+    color: #f58225;
+  }
+  div {
+    font-size: large;
+    margin-left: 24px;
+    color: #6b737c;
+  }
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #babfc4;
+  margin-right: 10px;
+  margin-left: 20px;
+
+  svg {
+    margin-bottom: 10px;
+  }
+`;
 function Vote() {
-  return <div>좋아요 클릭 / 표시</div>;
+  return (
+    <div>
+      <Container>
+        <button>
+          <GoTriangleUp />
+        </button>
+        <div>0</div>
+        <button>
+          <GoTriangleDown />
+        </button>
+      </Container>
+      <IconContainer>
+        <FiBookmark />
+        <RxCountdownTimer />
+      </IconContainer>
+    </div>
+  );
 }
 
 export default Vote;
