05e7c2e92b88c71b305da299e9a4747af2b6a015
tjddmssl <115976700+tjddmssl@users.noreply.github.com>
2023-03-02 16:38:35 +0900

    [front]feat: token (#89)
    token 로컬스토리지 저장, memberid 받아옴
    contributor:tjddmssl

--- client/src/redux/slice/userSlice.js
+++ client/src/redux/slice/userSlice.js
@@ -1,9 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 // TODO user key 값 전부 넣기
+const initialState = {
+  value: { isLogin: false, displayName: '', email: '' },
+};
+
 export const userSlice = createSlice({
   name: 'user',
-  initialState: { value: { isLogin: false, displayName: '', email: '' } },
+  initialState: initialState,
   reducers: {
     login: (state, action) => {
       state.value = action.payload;
